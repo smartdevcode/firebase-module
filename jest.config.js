119bc984dfad7457530d22ed759131c22442221f
SeniorDevCode <senior.dev.software@gmail.com>
2020-07-14 19:08:31 +0000

    chore(deps): update optionaldependency firebase-admin to v9

--- jest.config.js
+++ jest.config.js
@@ -2,20 +2,9 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: [
     'lib/**/*.js',
-    '!lib/plugins/**/*.js',
+    '!lib/plugins/*.js',
     '!lib/serverMiddleware/*.js',
     '!lib/templates/*.js'
   ],
-  moduleFileExtensions: [
-    'ts',
-    'js',
-    'json'
-  ],
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.js$': 'babel-jest',
-  },
-  transformIgnorePatterns: [
-    'node_modules'
-  ],
+  testEnvironment: 'node'
 }
