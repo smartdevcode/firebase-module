f089757b2ec77e515f0825f27d0f52817d27b0df
SeniorDevCode <senior.dev.software@gmail.com>
2020-07-18 18:40:54 +0200

    refactor: remove serviceReady vars, check service directly

--- types/index.d.ts
+++ types/index.d.ts
@@ -101,6 +101,9 @@ export interface RemoteConfigServiceConfig extends ServiceConfig {
 }
 
 export interface FirebaseModuleConfiguration {
+  legacyMode?: boolean,
+  injectModule?: boolean,
+  lazy?: boolean,
   config:
     | {
         [envKey: string]: FirebaseConfiguration
@@ -123,6 +126,7 @@ export interface FirebaseModuleConfiguration {
 
 declare module 'vue/types/vue' {
   interface Vue {
+    // LegacyMode
     $fireStore: firebase.firestore.Firestore
     $fireStoreObj: typeof firebase.firestore
     $fireDb: firebase.database.Database
@@ -142,6 +146,19 @@ declare module 'vue/types/vue' {
     $firePerfObj: typeof firebase.performance
     $fireConfig: firebase.remoteConfig.RemoteConfig
     $fireConfigObj: typeof firebase.remoteConfig
+    // From v7+
+    $fireObj: typeof firebase
+    $fire: {
+      auth: firebase.auth.Auth
+      realtimeDb: firebase.database.Database
+      firestore: firebase.firestore.Firestore
+      storage: firebase.storage.Storage
+      functions: firebase.functions.Functions
+      messaging: firebase.messaging.Messaging
+      performance: firebase.performance.Performance
+      analytics: firebase.analytics.Analytics
+      remoteConfig: firebase.remoteConfig.RemoteConfig
+    }
   }
 }
 
@@ -151,6 +168,7 @@ declare module '@nuxt/vue-app' {
   }
 
   interface NuxtAppOptions {
+    // LegacyMode
     $fireStore: firebase.firestore.Firestore
     $fireStoreObj: typeof firebase.firestore
     $fireDb: firebase.database.Database
@@ -170,6 +188,19 @@ declare module '@nuxt/vue-app' {
     $firePerfObj: typeof firebase.performance
     $fireConfig: firebase.remoteConfig.RemoteConfig
     $fireConfigObj: typeof firebase.remoteConfig
+     // From v7+
+     $fireObj: typeof firebase
+     $fire: {
+       auth: firebase.auth.Auth
+       realtimeDb: firebase.database.Database
+       firestore: firebase.firestore.Firestore
+       storage: firebase.storage.Storage
+       functions: firebase.functions.Functions
+       messaging: firebase.messaging.Messaging
+       performance: firebase.performance.Performance
+       analytics: firebase.analytics.Analytics
+       remoteConfig: firebase.remoteConfig.RemoteConfig
+     }
   }
 }
 
@@ -180,6 +211,7 @@ declare module '@nuxt/types' {
   }
 
   interface NuxtAppOptions {
+    // LegacyMode
     $fireStore: firebase.firestore.Firestore
     $fireStoreObj: typeof firebase.firestore
     $fireDb: firebase.database.Database
@@ -199,11 +231,25 @@ declare module '@nuxt/types' {
     $firePerfObj: typeof firebase.performance
     $fireConfig: firebase.remoteConfig.RemoteConfig
     $fireConfigObj: typeof firebase.remoteConfig
+     // From v7+
+     $fireObj: typeof firebase
+     $fire: {
+       auth: firebase.auth.Auth
+       realtimeDb: firebase.database.Database
+       firestore: firebase.firestore.Firestore
+       storage: firebase.storage.Storage
+       functions: firebase.functions.Functions
+       messaging: firebase.messaging.Messaging
+       performance: firebase.performance.Performance
+       analytics: firebase.analytics.Analytics
+       remoteConfig: firebase.remoteConfig.RemoteConfig
+     }
   }
 }
 
 declare module 'vuex/types/index' {
   interface Store<S> {
+     // LegacyMode
     readonly $fireStore: firebase.firestore.Firestore
     $fireStoreObj: typeof firebase.firestore
     $fireDb: firebase.database.Database
@@ -223,6 +269,19 @@ declare module 'vuex/types/index' {
     $firePerfObj: typeof firebase.performance
     $fireConfig: firebase.remoteConfig.RemoteConfig
     $fireConfigObj: typeof firebase.remoteConfig
+     // From v7+
+     $fireObj: typeof firebase
+     $fire: {
+       auth: firebase.auth.Auth
+       realtimeDb: firebase.database.Database
+       firestore: firebase.firestore.Firestore
+       storage: firebase.storage.Storage
+       functions: firebase.functions.Functions
+       messaging: firebase.messaging.Messaging
+       performance: firebase.performance.Performance
+       analytics: firebase.analytics.Analytics
+       remoteConfig: firebase.remoteConfig.RemoteConfig
+     }
   }
 }
 
